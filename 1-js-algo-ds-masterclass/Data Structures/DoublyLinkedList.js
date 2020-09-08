d14870e6548f3e418da6d13722a02e370f608012
MisterRK <robert.keller22@gmail.com>
2020-09-07 23:55:14 -0400

    unshift() for DoublyLinkedList

--- 1-js-algo-ds-masterclass/Data Structures/DoublyLinkedList.js
+++ 1-js-algo-ds-masterclass/Data Structures/DoublyLinkedList.js
@@ -59,4 +59,19 @@ class DoublyLinkedList {
 		this.length--;
 		return oldHead;
 	}
+
+	//adds a node to the beginning of a DoublyLinkedList
+	unshift(val) {
+		let newNode = new Node(val)
+		if ( this.length === 0 ){
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this
+	}
 }
