06f911329f0cf0fb627d8ca40e42ba14a9afed02
MisterRK <robert.keller22@gmail.com>
2020-09-20 00:14:39 -0400

    insert() method...iteratively

--- 1-js-algo-ds-masterclass/Data Structures/BinaryTrees.js
+++ 1-js-algo-ds-masterclass/Data Structures/BinaryTrees.js
@@ -1,17 +1,44 @@
-
 class Node {
-      constructor(value){
-         this.value = value;
-         this.left = null;
-         this.right = null;
-      }
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
 }
 
 //creates an empty Binary Search Treet
 class BinarySearchTree {
-   constructor(){
-      this.root = null;
-   }
+	constructor() {
+		this.root = null;
+	}
+
+	// inserts a node at the correct location in the tree.
+	insert(value) {
+		let newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+			return this;
+		} else {
+			let current = this.root;
+			while (true) {
+				if (value < current.value) {
+					if (current.left === null) {
+						current.left = newNode;
+						return this;
+					} else {
+						current = current.left;
+					}
+				} else if (value > current.value) {
+					if (current.right === null) {
+						current.right = newNode;
+						return this;
+					} else {
+						current = current.right;
+					}
+				}
+			}
+		}
+	}
 }
 
-
+let tree = new BinarySearchTree();
