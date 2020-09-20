1b8a543267e856d954ef46710bc808d0cb497572
MisterRK <robert.keller22@gmail.com>
2020-09-19 23:32:37 -0400

    Basic structure for tree

---
+++ 1-js-algo-ds-masterclass/Data Structures/BinaryTrees.js
@@ -0,0 +1,17 @@
+
+class Node {
+      constructor(value){
+         this.value = value;
+         this.left = null;
+         this.right = null;
+      }
+}
+
+//creates an empty Binary Search Treet
+class BinarySearchTree {
+   constructor(){
+      this.root = null;
+   }
+}
+
+
