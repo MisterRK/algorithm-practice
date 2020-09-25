e8907b562b70d8c104bde2600889ed9099074755
MisterRK <robert.keller22@gmail.com>
2020-09-25 14:47:06 -0400

    Breadth first search on Binary Tree class

--- 1-js-algo-ds-masterclass/Data Structures/BinaryTrees.js
+++ 1-js-algo-ds-masterclass/Data Structures/BinaryTrees.js
@@ -41,20 +41,53 @@ class BinarySearchTree {
 	}
 
 	//check a binary tree for a certain value
-	find(value){
-		if(this.root === value) return false;
+	find(value) {
+		// check if the list is empty
+		if (this.root === null) return false;
+		// set up to look at a value and move through the tree
 		let current = this.root;
 		let found = false;
-		while(current && !found){
-			if(value<current.value){
+		while (current && !found) {
+			// check if we should look left(less) or right(more)
+			if (value < current.value) {
 				current = current.left;
-			}else if (value > current.value){
+			} else if (value > current.value) {
 				current = current.right;
 			} else {
-				found = true}
+				// if we find the value exit the loop
+				found = true;
+			}
 		}
-		if(!found) return false;
+		// loop also exits if there is nothing to look at
+		if (!found) return false;
 		return !!current;
+	}
+
+	// Breadth First search of a tree
+
+	// create a queue ( this can be an array) and a variable to store the values of nodes visited
+	// place the root node inot the queue
+	// Loop as long as there is anything in the queue
+	// Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+	// if there is a left property one the node dequeued - add it to the queue
+	// if there is a right property on the node dequeued - add it to the queue
+	// Return the variable that stores the values
+
+	BFS() {
+		debugger;
+		let queue = []; // queue will store the nodes we will visit
+		let visited = []; // visited will sotre the nodes we HAVE visited
+		let current = this.root; // start at the root
+
+		queue.push(current); // add the root to the nodes we will visit
+
+		while (queue.length) {
+			current = queue.shift(); // we are looking at the first node in the queue ... FIFO
+			visited.push(current.value);
+			if (current.left) queue.push(current.left);
+			if (current.right) queue.push(current.right);
+		}
+		return visited;
 	}
 }
 
---
+++ 1-js-algo-ds-masterclass/Searching/BFS.js
@@ -0,0 +1,18 @@
+// Breadth First search of a tree
+
+// create a queue ( this can be an array) and a variable to store the values of nodes visited
+// place the root node inot the queue
+// Loop as long as there is anything in the queue
+   // Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+   // if there is a left property one the node dequeued - add it to the queue
+   // if there is a right property on the node dequeued - add it to the queue
+// Return the variable that stores the values
+
+function BFS(tree){
+   let queue = []
+   let visited = []
+   tree.root
+   while (queue){
+
+   }
+}
